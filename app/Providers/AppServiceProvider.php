696fe64ec6540bd97bc1a1a409c7634e115b8fae
gyarmex <gyarmex@gmail.com>
2025-08-22 06:57:12 +0900

    Guard Pail registration (local + class_exists) and remove from config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
+        if ($this->app->environment('local') && class_exists(\Laravel\Pail\PailServiceProvider::class)) {
             $this->app->register(\Laravel\Pail\PailServiceProvider::class);
         }
     }
