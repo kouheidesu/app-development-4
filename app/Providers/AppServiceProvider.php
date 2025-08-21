d7cf700819b07ca7d85c85134bd1eb01560090ef
gyarmex <gyarmex@gmail.com>
2025-08-22 07:02:48 +0900

    pailをコメントアウト

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local') && class_exists(\Laravel\Pail\PailServiceProvider::class)) {
-            $this->app->register(\Laravel\Pail\PailServiceProvider::class);
-        }
+        // if ($this->app->environment('local') && class_exists(\Laravel\Pail\PailServiceProvider::class)) {
+        //     $this->app->register(\Laravel\Pail\PailServiceProvider::class);
+        // }
     }
 
     /**
